24cbdb8cfa5c450187a6f9d24b48361ad6aec81b
t16Bennett <t16Bennett@gmail.com>
2019-11-02 16:21:10 -0600

    DP working

--- src/astronomyDP19/FindMaxEvents.java
+++ src/astronomyDP19/FindMaxEvents.java
@@ -13,7 +13,7 @@ public class FindMaxEvents {
 		int[][] table = new int[n][2*n-1];
 		for(int i = 0;i<n;i++) {
 			for(int j = 0;j<2*n-1;j++) {
-				if(j>=n-i && j<=n+i) {
+				if(j>=n-1-i && j<=n-1+i) {
 					table[i][j] = 0;
 				}
 				else {
@@ -21,26 +21,29 @@ public class FindMaxEvents {
 				}
 			}
 		}
+		System.out.println(table[0][n-1]);
 		table[n-1][eventCoordinates.get(n-1)+n-1] = 1;
 		for(int i = n-2;i>=0;i--) {
 			System.out.println("i " +i);
-			for(int j = n-i;j<=n+i;j++) {
+			for(int j = n-1-i;j<=n-1+i;j++) {
 				System.out.println("j " +j);
-				int max = -1;
+				int max = 0;
+				boolean hitMax = false;
 				for(int k = -1; k < 2; k++) {
 					if(j+k>0 && j+k<2*n-1) {
 						int temp = table[i+1][j+k];
 						if(temp>max) {
-							System.out.println("hit");
+							System.out.println("hit " + k);
+							hitMax = true;
 							max = temp;
 						}
 					}
 				}
 				table[i][j] = max;
-				System.out.println("set" + table[i][j]);
-				if(j == eventCoordinates.get(i)+n-1) {
+				if(j == eventCoordinates.get(i)+n-1 && hitMax) {
 					table[i][j] = table[i][j] + 1;
 				}
+				System.out.println("set" + table[i][j]);
 			}
 		}
 		return table[0][n-1];
