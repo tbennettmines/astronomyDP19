3d7966ad82369c251f511dc8049edb9acf23189c
t16Bennett <t16Bennett@gmail.com>
2019-11-02 21:58:34 -0600

    Still odd

--- src/astronomyDP19/FindMaxEvents.java
+++ src/astronomyDP19/FindMaxEvents.java
@@ -13,7 +13,7 @@ public class FindMaxEvents {
 		int[][] table = new int[n][2*n-1];
 		for(int i = 0;i<n;i++) {
 			for(int j = 0;j<2*n-1;j++) {
-				if(j>n-i && j<n+i) {
+				if(j>=n-i && j<=n+i) {
 					table[i][j] = 0;
 				}
 				else {
@@ -23,17 +23,21 @@ public class FindMaxEvents {
 		}
 		table[n-1][eventCoordinates.get(n-1)+n-1] = 1;
 		for(int i = n-2;i>=0;i--) {
-			for(int j = 0;j<2*n-1;j++) {
+			System.out.println("i " +i);
+			for(int j = n-i;j<=n+i;j++) {
+				System.out.println("j " +j);
 				int max = -1;
 				for(int k = -1; k < 2; k++) {
 					if(j+k>0 && j+k<2*n-1) {
 						int temp = table[i+1][j+k];
 						if(temp>max) {
+							System.out.println("hit");
 							max = temp;
 						}
 					}
 				}
 				table[i][j] = max;
+				System.out.println("set" + table[i][j]);
 				if(j == eventCoordinates.get(i)+n-1) {
 					table[i][j] = table[i][j] + 1;
 				}
